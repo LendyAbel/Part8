95e7eff6c6357a4a099ef08dbe60c196a623e9fd
LendyAbel <lendyabel93@gmail.com>
2025-08-29 12:56:32 +0200

    Implementing Routes

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,26 +1,33 @@
-import { useState } from "react";
-import Authors from "./components/Authors";
-import Books from "./components/Books";
-import NewBook from "./components/NewBook";
+import { Routes, Route, Link, Navigate } from 'react-router'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
 
 const App = () => {
-  const [page, setPage] = useState("authors");
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        <Link to={'/authors'}>
+          <button>authors</button>
+        </Link>
+        <Link to={'/books'}>
+          <button>books</button>
+        </Link>
+        <Link to={'/books/newbook'}>
+          <button>add book</button>
+        </Link>
       </div>
 
-      <Authors show={page === "authors"} />
+      <Routes>
+        <Route path='/' element={<Navigate to={'/authors'} replace/>} />
+        <Route path='/authors' element={<Authors />} />
+        <Route path='/books' element={<Books />} />
+        <Route path='/books/newbook' element={<NewBook />} />
+      </Routes>
 
-      <Books show={page === "books"} />
-
-      <NewBook show={page === "add"} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
